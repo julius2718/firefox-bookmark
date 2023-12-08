648147b903cd59ccdf2f228d947a575914b7b754
julius2718 <yuhgon0928@gmail.com>
2023-12-08 18:49:21 +0100

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# firefox-bookmark
-Alfred workflow to search and open Firefox bookmarks
+# Firefox bookmark fetching script for Alfred.app
+
+Python script for an Alfred workflow to search and open Firefox bookmarks.
+
+## Installation
+
+### Prerequisites
+
+You need to have Python installed on your machine.
+You also need to install additional packages specified in the **Requirements** section, if you haven't already done so.
+
+### Python script
+
+Clone this repository into a suitable location.
+You can also download ZIP of this repository if you prefer.
+
+### Alfred workflow
+
+Create an Alfred Workflow inside the Alfred application.
+Choose "Script Filter" as the input and copy-paste the following script into the "Script" field.
+
+```zsh
+cd $script_root_path
+
+$python_executable ./scr/main.py
+```
+
+Then create two Environment Variables named `script_root_path` and `python_executable` in the workflow editor.
+You should set the values for these variables according to your environment.
+
+You can also replace `$script_root_path` and `$python_executable` in the script with appropriate values, instead of creating variables.
+
+Finally, create an "Open URL" action and connect it to the Script Filter input.
+Enter `{query}` into the URL field in the object configuration and save.
+
+## Requirements
+
+You need to have the following additional Python package installed.
+
+- `lz4`: for LZ4 decompression purposes (<https://github.com/python-lz4/python-lz4>).
+
+## Future works
+
+- Publish the workflow as a bundle in the Alfred Gallery, if this tool gets enough attention.
+
+---
+
+Alfred is a registered trademark of Running with Crayons Ltd.
+
+Firefox is a trademark of the Mozilla Foundation in the U.S. and other countries.
+
+All the trademarks in this repository are used only to refer to and/or link to the original programs, products, services, and technologies.
+
---
+++ scr/main.py
@@ -0,0 +1,114 @@
+import lz4.block
+import json
+
+import os
+import sys
+
+
+def get_firefox_bookmarks():
+    # detect home directory of the user
+    home = os.path.expanduser("~")
+
+    # detect the path to the database file
+    ## https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data
+    path = os.path.join(home, "Library/Application Support/Firefox/Profiles/")
+
+    ## search in the subdirectories for bookmarkbackups directory
+    backup_path = None
+    for root, dirs, files in os.walk(path):
+        for name in dirs:
+            if name == "bookmarkbackups":
+                backup_path = os.path.join(root, name)
+                break
+
+    ## raise an error if the backup path is not found
+    if not backup_path:
+        raise FileNotFoundError("Bookmark backup path not found")
+
+    # detect the latest backup file
+    ## https://stackoverflow.com/questions/39327032/how-to-get-the-latest-file-in-a-folder-using-python
+    list_of_backups = [os.path.join(backup_path, f) for f in os.listdir(backup_path)]
+    latest = max(list_of_backups, key=os.path.getctime)
+
+    # read the latest backup file
+    with open(latest, "rb") as f:
+        f.read(8)
+        data = f.read()
+        json_str = lz4.block.decompress(data).decode("utf-8")
+
+    # parse json data
+    data = json.loads(json_str)
+
+    return data
+
+
+# # detect home directory of the user
+# home = os.path.expanduser("~")
+#
+# # detect the path to the database file
+# ## https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data
+# path = os.path.join(home, "Library/Application Support/Firefox/Profiles/")
+#
+# ## search in the subdirectories for bookmarkbackups directory
+# backup_path = None
+# for root, dirs, files in os.walk(path):
+#     for name in dirs:
+#         if name == "bookmarkbackups":
+#             backup_path = os.path.join(root, name)
+#             break
+#
+# ## raise an error if the backup path is not found
+# if not backup_path:
+#     raise FileNotFoundError("Bookmark backup path not found")
+#
+# # detect the latest backup file
+# ## https://stackoverflow.com/questions/39327032/how-to-get-the-latest-file-in-a-folder-using-python
+# list_of_backups = [os.path.join(backup_path, f) for f in os.listdir(backup_path)]
+# latest = max(list_of_backups, key=os.path.getctime)
+#
+# # read the latest backup file
+# with open(latest, "rb") as f:
+#     f.read(8)
+#     data = f.read()
+#     json_str = lz4.block.decompress(data).decode("utf-8")
+#
+# # parse json data
+# data = json.loads(json_str)
+
+
+# extract bookmarks
+## recursive function to extract bookmarks
+def extract_bookmarks(data):
+    if "children" in data.keys():
+        for child in data["children"]:
+            yield from extract_bookmarks(child)
+    elif data["typeCode"] == 1:
+        yield data
+    else:
+        pass
+
+
+def main():
+    data = get_firefox_bookmarks()
+    bookmarks = list(extract_bookmarks(data))
+
+    alfred_results = []
+    for bookmark in bookmarks:
+        item = {
+            "uid": bookmark["guid"],
+            "title": bookmark["title"],
+            "subtitle": bookmark["uri"],
+            "arg": bookmark["uri"],
+            "autocomplete": bookmark["title"],
+            "icon": {"path": "./icon.png"},
+        }
+        alfred_results.append(item)
+
+    # output the results
+    response = json.dumps({"items": alfred_results})
+
+    sys.stdout.write(response)
+
+
+if __name__ == "__main__":
+    main()
